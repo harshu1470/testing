bba1d40915f3b6152dd9eae5c45fa60fcb77db42
Harshit Sharma <harshit1470@gmail.com>
2021-01-28 17:55:39 +0530

    updated

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
                         <li>About testing
                         <ul>
                                 <li>About website</li>
-                                <li>Goals</li>
+                                <li>Future Plans</li>
                                 <li>Latest Offers</li>
                                 <li>Achievements
                                         <ul>
